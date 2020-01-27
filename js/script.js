93d9f70d91063224c9401ec5bd2d57582bbf937e
Diaq <43369772+imcrazydia@users.noreply.github.com>
2020-01-27 10:58:14 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// When the user clicks on div, open the popup
+function Function1() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+  function Function2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+  }
+
+  function Function3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+  }
+
+  function Function4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+  }
+
+  function Function5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show");
+  }
+
+  function Function6() {
+    var popup = document.getElementById("myPopup6");
+    popup.classList.toggle("show");
+  }
+
+  function Function7() {
+    var popup = document.getElementById("myPopup7");
+    popup.classList.toggle("show");
+  }
